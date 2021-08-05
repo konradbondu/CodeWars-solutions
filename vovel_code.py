b369eb392c9a9c25f1ffd7e89f1b1967c9126185
Konrad Bondarczuk <konrad.bondarczuk@gmail.com>
2021-08-05 07:40:04 +0200

    add solution 'vovel code'

---
+++ vovel_code.py
@@ -0,0 +1,36 @@
+# Step 1: Create a function called encode() to replace all the lowercase vowels in a given string with numbers
+# according to the following pattern:
+#
+# a -> 1
+# e -> 2
+# i -> 3
+# o -> 4
+# u -> 5
+# For example, encode("hello") would return "h2ll4". There is no need to worry about uppercase vowels in this kata.
+#
+# Step 2: Now create a function called decode() to turn the numbers back into vowels according to the same pattern
+# shown above.
+#
+# For example, decode("h3 th2r2") would return "hi there".
+#
+# For the sake of simplicity, you can assume that any numbers passed into the function will correspond to vowels.
+
+
+def encode(st):
+    dict_code = {'a': '1', 'e': '2', 'i': '3', 'o': '4', 'u': '5'}
+    str_list = list(st)
+
+    for i in range(len(str_list)):
+        if str_list[i] in dict_code:
+            str_list[i] = dict_code[str_list[i]]
+    return "".join(str_list)
+
+
+def decode(st):
+    dict_code = {'1': 'a', '2': 'e', '3': 'i', '4': 'o', '5': 'u'}
+    str_list = list(st)
+
+    for i in range(len(str_list)):
+        if str_list[i] in dict_code:
+            str_list[i] = dict_code[str_list[i]]
+    return "".join(str_list)
