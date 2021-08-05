25aa14aa2f674deb20f8333704dd578f1e77868f
Konrad Bondarczuk <konrad.bondarczuk@gmail.com>
2021-08-05 07:45:58 +0200

    add solution 'Scramblies'

---
+++ scramblies.py
@@ -0,0 +1,21 @@
+# Complete the function scramble(str1, str2) that returns true if a portion of str1 characters can be rearranged to match str2, otherwise returns false.
+#
+# Notes:
+#
+# Only lower case letters will be used (a-z). No punctuation or digits will be included.
+# Performance needs to be considered
+# Input strings s1 and s2 are null terminated.
+# Examples
+# scramble('rkqodlw', 'world') ==> True
+# scramble('cedewaraaossoqqyt', 'codewars') ==> True
+# scramble('katas', 'steak') ==> False
+
+
+from collections import Counter
+
+
+def scramble(s1, s2):
+    # Counter basically creates a dictionary of counts and letters
+    # Using set subtraction, we know that if anything is left over,
+    # something exists in s2 that doesn't exist in s1
+    return len(Counter(s2) - Counter(s1)) == 0
